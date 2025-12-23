f4bb0eeb6e651d0c3fc8c66c3742002ff4527df9
Vinay-Neuro <dr.vinay.neuro@gmail.com>
2025-12-23 15:06:17 +0530

    Update metacleanr-package.R

--- R/metacleanr-package.R
+++ R/metacleanr-package.R
@@ -1,6 +1,17 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
+#' metacleanr: Validate and harmonize meta-analysis datasets
+#'
+#' Tools to validate, harmonize and (optionally) fix common problems in
+#' meta-analysis and network meta-analysis datasets.
+#'
+#' The main entry point for users is \code{\link{metaclean}} which runs a
+#' battery of checks and returns a list containing the cleaned data and a
+#' diagnostics report. Utilities include network/subnetwork detection,
+#' missing-value suggestions, and NMA-specific prechecks.
+#'
+#' @docType package
+#' @name metacleanr
+#' @aliases metacleanr-package
+#' @keywords package
+#' @author Vinay Suresh <vinay.suresh@bnc.ox.ac.uk>
+#' @seealso \code{\link{metaclean}}, \code{\link{detect_subnetworks}}
 NULL
